56651e57d5f85183e6f6190305e9b13590b2f499
shane <spp935@gmail.com>
2017-06-29 12:16:41 +1000

    added packes and exports

---
+++ app/index.js
@@ -0,0 +1,13 @@
+var rules = require("../app/rules");
+var getPlugins = require("../app/plugins");
+var resolve = require("../app/resolve");
+var getOutput = require("../app/output");
+var getEntry = require("../app/entry");
+
+module.exports = {
+  rules,
+  getPlugins,
+  resolve,
+  getOutput,
+  getEntry
+}
